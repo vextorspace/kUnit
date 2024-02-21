0651a5909da397fcbd49d8b3615cd0019e12b9d3
Douglas Ronne <vextorspace@gmail.com>
2024-02-21 14:33:44 -0400

    removed duplication in throwing exception

--- src/main/kotlin/assertions/Assertions.kt
+++ src/main/kotlin/assertions/Assertions.kt
@@ -3,15 +3,13 @@ package assertions
 object Assertions {
     fun assertTrue(expression: Boolean?) {
         if(expression == null) {
-            throw AssertionFailedException("should have been true but was null")
-        }
-
-        if(expression.not()) {
-            throw AssertionFailedException("should have been true but was false")
+            throwException("should have been true but was null")
+        } else if(expression.not()) {
+            throwException("should have been true but was false")
         }
     }
 
-    fun assertFalse(expression: Boolean?) {
-
+    private fun throwException(message: String) {
+        throw AssertionFailedException(message)
     }
 }
